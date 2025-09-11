bb7782007820e9a70d5b28a3f574f62603552691
Thubelihle Dlamini <thubelihledlamini88@gmail.com>
2025-09-11 10:08:53 +0200

    Create future_age_calculator.py

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,7 @@
+current_age = input("How old are you? ")
+
+# The input() function returns a string, so we need to cast it to an integer
+# to perform arithmetic operations.
+age_in_2050 = int(current_age) + 27
+
+print(f"In 2050, you will be {age_in_2050} years old.")
